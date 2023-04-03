32532821a95c10f69aa6aaec25c8d6e949481fb0
shayashkenazi <shayas9529@gmail.com>
2023-04-03 19:06:53 +0300

    greengurdian init spring boot

---
+++ greenguardian/src/main/java/com/mta/greenguardian/GreenguardianApplication.java
@@ -0,0 +1,13 @@
+package com.mta.greenguardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenguardianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenguardianApplication.class, args);
+	}
+
+}
---
+++ greenguardian/src/test/java/com/mta/greenguardian/GreenguardianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mta.greenguardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenguardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
