54f609155c8ea28d26dc8183c28836b46e6f3220
shayashkenazi <shayas9529@gmail.com>
2023-04-03 20:05:28 +0300

    properties update

--- greenguardian/build.gradle.kts
+++ greenguardian/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("mysql:mysql-connector-java:8.0.27")
 }
 
 tasks.withType<Test> {
