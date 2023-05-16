ca7c512d3e063aed5ac91b067735dcce99ec2813
Ido Fassberg <idofassberg@gmail.com>
2023-05-16 20:39:19 +0300

    delete irrelevant projects

--- greenguardian/src/main/java/com/mta/greenguardian/model/plant/UserPlantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mta.greenguardian.model.plant;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserPlantRepository extends CrudRepository<UserPlant,Integer> {
-
-}
