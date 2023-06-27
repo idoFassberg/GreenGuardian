9db184b63e854c539fa9ae94e1531bda5223a160
Ido Fassberg <idofassberg@gmail.com>
2023-06-27 20:54:35 +0300

    add

--- soil_measurement/soil_measurement.ino
+++ soil_measurement/soil_measurement.ino
@@ -1,14 +1,6 @@
 #include <Arduino.h>
-#if defined(ESP32)
-#include <WiFi.h>
-#include <FirebaseESP32.h>
-#elif defined(ESP8266) // This is defined
-#include <ESP8266WiFi.h>
 #include <FirebaseESP8266.h>
-#elif defined(ARDUINO_RASPBERRY_PI_PICO_W)
-#include <WiFi.h>
-#include <FirebaseESP8266.h>
-#endif
+#include <WiFiManager.h> // Connect to Wi-Fi using html config file
 
 // Provide the token generation process info.
 #include <addons/TokenHelper.h>
@@ -16,60 +8,53 @@
 // Provide the RTDB payload printing info and other helper functions.
 #include <addons/RTDBHelper.h>
 
-const char* ssid = "Fassberg_2.4"; // Wi-Fi name
-const char* password = "0546366753";
 const char* webAPIKey = "AIzaSyBfcRrSCqfqCui2Fkt1DlTHaTZTBx0sAIQ";
 const char* databaseUrl = "https://greenguardian-app-default-rtdb.europe-west1.firebasedatabase.app/";
-const char* userEmail = "test@gmail.com";
-const char* userPassword = "123456";
 
 FirebaseData fbdo;
-FirebaseData fbdo2;
 FirebaseAuth auth;
 FirebaseConfig config;
 #define soilMoisturePin A0
 String macAddress;
 
-void initializeWiFiConnection()
-{
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  } 
+WiFiManagerParameter customFirebaseEmail("firebaseEmail", "Firebase User Email", "", 40);
+WiFiManagerParameter customFirebasePassword("firebasePassword", "Firebase User Password", "", 40);
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("NodeMCU IP address: " + WiFi.localIP().toString());
+
+void accessWiFiUsingManager(WiFiManager& wifiManager)
+{
+  // Add custom parameters to the configuration page
+  wifiManager.addParameter(&customFirebaseEmail);
+  wifiManager.addParameter(&customFirebasePassword);
+
+  // set custom ip for portal
+  wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0)); // new func name instead of setAPConfig
+  wifiManager.setTimeout(120);
+  wifiManager.autoConnect("GreenGuardian"); // blocking until connected
+  Serial.println("Connected.");
 }
 
 void setup()
 {
   Serial.begin(9600);
+  WiFiManager wifiManager;
 
-  // initialize Led
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-
-  initializeWiFiConnection();
+  accessWiFiUsingManager(wifiManager);
 
   // Retrieve and print the MAC address
   macAddress = WiFi.macAddress();
   macAddress.replace(":", "");
-  Serial.print("MAC Address: ");
-  Serial.println(macAddress);
+  Serial.println("MAC Address: " + macAddress);
 
   config.api_key = webAPIKey;
   config.database_url = databaseUrl;
-  auth.user.email = userEmail;
-  auth.user.password = userPassword;
+  auth.user.email = customFirebaseEmail.getValue();
+  auth.user.password = customFirebasePassword.getValue();
   Firebase.begin(&config, &auth);
 }
 
 unsigned long previousMillis = 0;
-const unsigned long interval = 5000; // 5 second interval
+const unsigned long interval = 10000; // 10 second interval
 
 void loop()
 {
@@ -78,21 +63,14 @@ void loop()
   // Create a time delay using millis()
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) { // create delay without using delay()
-      previousMillis = currentMillis;
+  if (Firebase.ready() && (currentMillis - previousMillis >= interval)) { // create delay without using delay()
+    previousMillis = currentMillis;
       
-    if (Firebase.ready())
-    {
-      FirebaseJson json;
-      json.set("humidity", soilMoisture);
+    FirebaseJson json;
+    json.set("humidity", soilMoisture);
 
-      // Set the data in Firebase under the MAC address node
-      String path = "/RealTimeData/" + macAddress;
-      Serial.println(Firebase.RTDB.setJSON(&fbdo, path.c_str(), &json) ? "Json sent successfully" : fbdo.errorReason().c_str());
-    }
-    else
-    {
-      Serial.println("firebase is not ready!");
-    }
+    // Set the data in Firebase under the MAC address node
+    String path = "/RealTimeData/" + macAddress;
+    Serial.println(Firebase.RTDB.setJSON(&fbdo, path.c_str(), &json) ? "Json sent successfully" : fbdo.errorReason().c_str());
   }
 }
