36bd4c48ea5a74897289ea6eda68f913cde7bc1f
Shelly Golden <shelly.golden@gmail.com>
2023-07-30 19:13:38 +0300

    Continue building the chat

--- AndroidStudioProjects/app/src/main/java/com/mta/greenguardianapplication/MainChatActivity.java
+++ AndroidStudioProjects/app/src/main/java/com/mta/greenguardianapplication/MainChatActivity.java
@@ -1,22 +1,30 @@
 package com.mta.greenguardianapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
+import android.util.Printer;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.data.model.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.mta.greenguardianapplication.databinding.ActivityMainChatBinding;
 import com.mta.greenguardianapplication.utilities.Constants;
 import com.mta.greenguardianapplication.utilities.PreferenceManager;
 
-import kotlin.jvm.internal.PackageReference;
 
 public class MainChatActivity extends AppCompatActivity {
 
@@ -31,17 +39,49 @@ public class MainChatActivity extends AppCompatActivity {
         binding = ActivityMainChatBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         preferenceManager = new PreferenceManager(getApplicationContext());
+        mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
         DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid());
         loadUserDetails(userRef);
+        /*getToken();*/
+
+    }
+
+    private void loadUserDetails(DatabaseReference userRef) {
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String name = dataSnapshot.child("name").getValue(String.class);
+                    binding.textName.setText(name);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+
+        });
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void getToken(){
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::updateToken);
+    }
+
+    private void updateToken(String token){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference =
+                database.collection(Constants.KEY_COLLECTION_USERS).document(
+                        preferenceManager.getString(Constants.KEY_USER_ID)
+                );
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(unused -> showToast("Token updated successfully"))
+                .addOnFailureListener(e -> showToast("Unable to update token"));
     }
 
 
-    private void loadUserDetails(DatabaseReference userRef){
-        /*binding.textName.setText(userRef.user);*/
-        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
-        /*byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
-        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-        binding.imageProfile.setImageBitmap(bitmap);*/
-    }
 }
---
+++ AndroidStudioProjects/app/src/main/java/com/mta/greenguardianapplication/model/User.java
@@ -0,0 +1,9 @@
+package com.mta.greenguardianapplication.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String name, image, email, token;
+}
+
+
