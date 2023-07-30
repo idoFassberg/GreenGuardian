a7ce6a966daba849e221e55a052690de69eb4c2b
Shelly Golden <shelly.golden@gmail.com>
2023-07-30 17:22:39 +0300

    Start building the chat

--- AndroidStudioProjects/app/src/main/java/com/mta/greenguardianapplication/MainChatActivity.java
+++ AndroidStudioProjects/app/src/main/java/com/mta/greenguardianapplication/MainChatActivity.java
@@ -7,6 +7,11 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
 
+import com.firebase.ui.auth.data.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.mta.greenguardianapplication.databinding.ActivityMainChatBinding;
 import com.mta.greenguardianapplication.utilities.Constants;
 import com.mta.greenguardianapplication.utilities.PreferenceManager;
@@ -17,6 +22,8 @@ public class MainChatActivity extends AppCompatActivity {
 
     private ActivityMainChatBinding binding;
     private PreferenceManager preferenceManager;
+    private FirebaseUser user;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +31,17 @@ public class MainChatActivity extends AppCompatActivity {
         binding = ActivityMainChatBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         preferenceManager = new PreferenceManager(getApplicationContext());
-        /*loadUserDetails();*/
+        user = mAuth.getCurrentUser();
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid());
+        loadUserDetails(userRef);
     }
 
-    /*private void loadUserDetails(){
+
+    private void loadUserDetails(DatabaseReference userRef){
+        /*binding.textName.setText(userRef.user);*/
         binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
         /*byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
         Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-        binding.imageProfile.setImageBitmap(bitmap);
-    }*/
+        binding.imageProfile.setImageBitmap(bitmap);*/
+    }
 }
