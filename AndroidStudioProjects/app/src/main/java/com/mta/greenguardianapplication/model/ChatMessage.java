805c4a2a7679be74cb65a253e2aa301c22db28d7
Shelly Golden <shelly.golden@gmail.com>
2023-08-06 19:26:20 +0300

    Continue to build the chat

--- AndroidStudioProjects/app/src/main/java/com/mta/greenguardianapplication/model/ChatMessage.java
+++ AndroidStudioProjects/app/src/main/java/com/mta/greenguardianapplication/model/ChatMessage.java
@@ -1,5 +1,23 @@
 package com.mta.greenguardianapplication.model;
 
+import java.util.Date;
+
 public class ChatMessage {
     public String senderId, receiverId, message, dateTime;
+    public Date dateObjct;
+
+    public ChatMessage() {
+        // Empty constructor required for Firebase
+    }
+
+    public ChatMessage(String senderId, String receiverId, String message, String dateTime) {
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.message = message;
+        this.dateTime = dateTime;
+    }
+
+    public String getTimestamp() {
+        return dateTime;
+    }
 }
