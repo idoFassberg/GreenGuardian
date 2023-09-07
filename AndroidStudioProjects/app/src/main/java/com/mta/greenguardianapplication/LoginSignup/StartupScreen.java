1e94727abc70e8242482d1b5472a6b8525f1dab1
shayashkenazi <shayas9529@gmail.com>
2023-09-07 15:17:05 +0300

    back button from aboutUs layout

--- AndroidStudioProjects/app/src/main/java/com/mta/greenguardianapplication/LoginSignup/StartupScreen.java
+++ AndroidStudioProjects/app/src/main/java/com/mta/greenguardianapplication/LoginSignup/StartupScreen.java
@@ -1,48 +1,33 @@
 package com.mta.greenguardianapplication.LoginSignup;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import androidx.core.content.ContextCompat;
-
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Context;
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
-import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
-import android.Manifest;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.mta.greenguardianapplication.R;
-
-import java.lang.ref.WeakReference;
 
 
 public class StartupScreen extends AppCompatActivity {
 
     Button aboutUs;
+
+    private AlertDialog aboutUsDialog;
 
     private final ActivityResultLauncher<String> requestPermissionLauncher =
             registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
@@ -68,18 +53,18 @@ public class StartupScreen extends AppCompatActivity {
         aboutUs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                android.app.AlertDialog.Builder builder= new android.app.AlertDialog.Builder(StartupScreen.this);
+                AlertDialog.Builder builder = new AlertDialog.Builder(StartupScreen.this);
                 View dialogView = getLayoutInflater().inflate(R.layout.dialog_about_us, null);
-
                 builder.setView(dialogView);
-                android.app.AlertDialog dialog = builder.create();
+                aboutUsDialog = builder.create();
 
-                if (dialog.getWindow() != null){
-                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+                if (aboutUsDialog.getWindow() != null) {
+                    aboutUsDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
                 }
-                dialog.show();
+                aboutUsDialog.show();
             }
         });
+
     }
 
     public void callLoginScreen(View view) {
@@ -150,4 +135,21 @@ public class StartupScreen extends AppCompatActivity {
         AlertDialog dialog = dialogBuilder.create();
         dialog.show();
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (aboutUsDialog != null && aboutUsDialog.isShowing()) {
+            aboutUsDialog.dismiss();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (aboutUsDialog != null && aboutUsDialog.isShowing()) {
+            aboutUsDialog.dismiss();
+        }
+    }
+
 }
